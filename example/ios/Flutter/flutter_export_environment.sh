b98681f643c06110e330d4b3b78fb70360feb96e
Richard Muragijimana <beastar457@gmail.com>
2022-09-25 06:05:03 +0200

    latest windows

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\SPENN\Documents\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\SPENN\Documents\personal\flipper\dev\flipper\open-sources\open_file\example"
+export "FLUTTER_APPLICATION_PATH=C:\Users\SPENN\Documents\flipper\open-sources\open_file\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
